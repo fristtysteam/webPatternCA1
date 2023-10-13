7d2517d2a0a802cb292cbf68eb985eb35c1dc5a5
Marco <fristtysteam@gmail.com>
2023-10-13 15:32:04 +0100

    second commit from me marco first one had my email as the message my bad , Book.java file

---
+++ src/main/java/business/Book.java
@@ -0,0 +1,87 @@
+package business;
+
+import java.util.Objects;
+
+public class Book{
+
+    private int bookID;
+    private String bookName;
+    private String author;
+    private String description;
+    private int quantity;
+
+    public Book() {
+    }
+
+    public Book(int bookID, String bookName, String author, String description, int quantity) {
+        this.bookID = bookID;
+        this.bookName = bookName;
+        this.author = author;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return getBookID() == book.getBookID() && getQuantity() == book.getQuantity() && Objects.equals(getBookName(), book.getBookName()) && Objects.equals(getAuthor(), book.getAuthor()) && Objects.equals(getDescription(), book.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookID(), getBookName(), getAuthor(), getDescription(), getQuantity());
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookID=" + bookID +
+                ", bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
