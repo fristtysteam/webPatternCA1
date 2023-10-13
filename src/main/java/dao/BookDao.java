00c11798a4bfd233ee1082c09b5d0fd03be6a684
playerzer0-ui <playerzero745@gmail.com>
2023-10-13 16:15:54 +0100

    update the BookDao class

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -2,18 +2,73 @@ package dao;
 
 import business.Book;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookDao extends Dao implements BookDaoInterface{
     @Override
     public List<Book> getAllBooks() {
-        System.out.println("what do I do");
+        List<Book> books = new ArrayList<>();
+        try{
+            String query = "SELECT * FROM books";
+            con = getConnection();
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+
+            while(rs.next()){
+                books.add(new Book(
+                        rs.getInt("bookID"),
+                        rs.getString("bookName"),
+                        rs.getString("author"),
+                        rs.getString("description"),
+                        rs.getInt("quantity")
+                ));
+            }
+
+            return books;
+        }
+        catch(SQLException se){
+            System.out.println(se.getMessage());
+            System.out.println("something went wrong");
+        }
+        finally {
+            freeConnection();
+        }
 
         return null;
     }
 
     @Override
     public Book getBookByID(int bookID) {
+        Book book = null;
+        try{
+            String query = "SELECT * FROM books WHERE bookID = ?";
+            con = getConnection();
+            ps = con.prepareStatement(query);
+            ps.setInt(1, bookID);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                book = new Book(
+                        rs.getInt("bookID"),
+                        rs.getString("bookName"),
+                        rs.getString("author"),
+                        rs.getString("description"),
+                        rs.getInt("quantity")
+                );
+            }
+
+            return book;
+        }
+        catch(SQLException se){
+            System.out.println(se.getMessage());
+            System.out.println("something went wrong");
+        }
+        finally {
+            freeConnection();
+        }
+
         return null;
     }
 }
