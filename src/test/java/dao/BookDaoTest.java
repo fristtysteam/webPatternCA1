3072c6f9a3597d679135d28ddfdff3a5116e4286
CheePheng <wildwoodywildwoody@outlook.com>
2023-10-14 14:45:34 +0100

    creating dao test file

---
+++ src/test/java/dao/BookDaoTest.java
@@ -0,0 +1,19 @@
+package dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookDaoTest {
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getBookByID() {
+    }
+
+    @Test
+    void updateBookQuantity() {
+    }
+}
