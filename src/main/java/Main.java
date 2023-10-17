27535da10c2287f131996706a09c2aee6c801539
playerzer0-ui <playerzero745@gmail.com>
2023-10-17 18:39:01 +0100

    change the main file to a blank slate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,8 @@
 
-import business.User;
-import dao.UserDao;
 
 public class Main {
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDao("library");
-        User u =userDao.registerUser("admin", "admin@gmail.com",
-                "rippleMMW1$", "address", "0231030213");
-
-        System.out.println(u);
-        User a = userDao.loginUser("admin@gmail.com", "rippleMMW1$");
-
-        System.out.println(a);
+        System.out.println("sdsdsd");
     }
 }
