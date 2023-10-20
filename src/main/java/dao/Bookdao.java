44dc5e0993ae65f037af914546afe5f5c4d6021d
CheePheng <wildwoodywildwoody@outlook.com>
2023-10-20 16:06:06 +0100

    Fix some error handling, and changed method

--- src/main/java/dao/Bookdao.java
+++ src/main/java/dao/Bookdao.java
@@ -2,12 +2,12 @@ package dao;
 
 import business.Book;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class Bookdao extends Dao implements BookDaoInterface {
 
@@ -15,54 +15,97 @@ public class Bookdao extends Dao implements BookDaoInterface {
         super(dbName);
     }
 
+    /**
+     * Get a list of all books in the library.
+     *
+     * @return A list of books.
+     */
     @Override
-    public List<Book> getAllBooks() throws SQLException {
-        String query = "SELECT * FROM books";
-        ResultSet rs = ps.executeQuery(query);
+    public List<Book> getAllBooks() {
         List<Book> books = new ArrayList<>();
-        while (rs.next()) {
-            Book book = new Book();
-            book.setBookID(rs.getInt("bookID"));
-            book.setBookName(rs.getString("bookName"));
-            book.setAuthor(rs.getString("author"));
-            book.setDescription(rs.getString("description"));
-            book.setQuantity(rs.getInt("quantity"));
-            books.add(book);
+
+        String query = "SELECT * FROM books";
+
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                books.add(new Book(
+                        rs.getInt("bookID"),
+                        rs.getString("bookName"),
+                        rs.getString("author"),
+                        rs.getString("description"),
+                        rs.getInt("quantity")
+                ));
+            }
+        } catch (SQLException e) {
+            // Handle or log the exception
+            e.printStackTrace();
         }
+
         return books;
     }
 
+    /**
+     * Get a book by its ID.
+     *
+     * @param bookID The ID of the book to retrieve.
+     * @return The Book object if found, otherwise null.
+     */
     @Override
-    public Book getBookByID(int bookID) throws SQLException {
+    public Book getBookByID(int bookID) {
+        Book book = null;
+
         String query = "SELECT * FROM books WHERE bookID = ?";
-        PreparedStatement ps = con.prepareStatement(query);
-        ps.setInt(1, bookID);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-            Book book = new Book();
-            book.setBookID(rs.getInt("bookID"));
-            book.setBookName(rs.getString("bookName"));
-            book.setAuthor(rs.getString("author"));
-            book.setDescription(rs.getString("description"));
-            book.setQuantity(rs.getInt("quantity"));
-            return book;
-        } else {
-            return null;
+
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+
+            ps.setInt(1, bookID);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    book = new Book(
+                            rs.getInt("bookID"),
+                            rs.getString("bookName"),
+                            rs.getString("author"),
+                            rs.getString("description"),
+                            rs.getInt("quantity")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            // Handle or log the exception
+            e.printStackTrace();
         }
+
+        return book;
     }
 
+    /**
+     * Update the quantity of a book in the library.
+     *
+     * @param bookID   The ID of the book to update.
+     * @param increase True if you want to increase the quantity, false to decrease.
+     * @return The number of rows affected by the update.
+     */
     @Override
-    public int updateBookQuantity(int bookID, boolean increase) throws SQLException {
-        String query = "UPDATE books SET quantity = ? WHERE bookID = ?";
-        PreparedStatement ps = con.prepareStatement(query);
-        int quantity = getBookByID(bookID).getQuantity();
-        if (increase) {
-            quantity++;
-        } else {
-            quantity--;
+    public int updateBookQuantity(int bookID, boolean increase) {
+        String query = increase ? "UPDATE books SET quantity = quantity + 1 WHERE bookID = ?"
+                : "UPDATE books SET quantity = quantity - 1 WHERE bookID = ?";
+        int rowsAffected = 0;
+
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+
+            ps.setInt(1, bookID);
+            rowsAffected = ps.executeUpdate();
+        } catch (SQLException e) {
+            // Handle or log the exception
+            e.printStackTrace();
         }
-        ps.setInt(1, quantity);
-        ps.setInt(2, bookID);
-        return ps.executeUpdate();
+
+        return rowsAffected;
     }
 }
