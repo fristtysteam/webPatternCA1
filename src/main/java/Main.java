30c5360c84f7c8a248b7eff2797ff5eae2e309cb
playerzer0-ui <playerzero745@gmail.com>
2023-10-23 14:39:11 +0100

    upgrade the messy hash to a Bcrypt, AES no need

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
-
+import org.mindrot.jbcrypt.BCrypt;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        String pass = "rippleMMW1$";
+        String hash = BCrypt.hashpw(pass, BCrypt.gensalt());
+        System.out.println(hash);
+        System.out.println(hash.length());
     }
 }
