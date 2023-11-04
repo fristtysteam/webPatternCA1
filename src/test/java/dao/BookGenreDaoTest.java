d462153235e72e3fac591f6e8fcbc8350ca8264a
Marco <fristtysteam@gmail.com>
2023-11-04 22:39:43 +0000

    final commit javadoc for my test class BookGenreDaoTest. -marco uploading group project after this.

--- src/test/java/dao/BookGenreDaoTest.java
+++ src/test/java/dao/BookGenreDaoTest.java
@@ -13,6 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class BookGenreDaoTest {
     private final BookGenreDao bookGenreDao = new BookGenreDao("testLibrary");
+    /**
+     * Test case for retrieving genres by a valid book ID.
+     * It asserts that the correct genres are returned for the given book ID.
+     */
     @Test
     void getGenreByBookID() {
 
@@ -22,6 +26,9 @@ class BookGenreDaoTest {
 
 
     }
+    /**
+     * Test case for retrieving genres by an invalid (negative) book ID .
+     */
     @Test
     void getGenreByBookID2() {
 
@@ -31,7 +38,10 @@ class BookGenreDaoTest {
 
 
     }
-
+    /**
+     * Test case for retrieving books by their genre.
+     * It asserts that the correct books are returned for the given genre name.
+     */
     @Test
     void getBooksByGenre() {
         List<Book> books = bookGenreDao.getBooksByGenre("comedy");
@@ -40,7 +50,10 @@ assertEquals(2,books.get(1).getBookID());
 assertEquals(4,books.get(2).getBookID());
 
 
-
+        /**
+         * Test case for retrieving books by their genre two.
+         * It asserts that the correct books are returned for the given genre name.
+         */
     }
     @Test
     void getBooksByGenre2() {
@@ -51,6 +64,10 @@ assertEquals(4,books.get(2).getBookID());
 
     }
 
+    /**
+     * Test case for adding genre to a book.
+     * It asserts that the correct genres are added to the books given book ID.
+     */
     @Test
     void addGenreToBook() {
 int books = bookGenreDao.addGenreToBook(5,1);
@@ -62,19 +79,32 @@ int books = bookGenreDao.addGenreToBook(5,1);
 
 
     }
+    /**
+     * Test case for adding genre to a book 2.
+     * It asserts that the correct genres are added to the books given book ID.
+     * Validates the wrong bookID.
+     */
     @Test
     void addGenreToBook2(){
         int books = bookGenreDao.addGenreToBook(50,1);
         assertEquals(0,books);
 
     }
+    /**
+     * Test case for adding genre to a book 3.
+     * It asserts that the correct genres are added to the books given book ID.
+     * Validates the wrong genreID
+     */
     @Test
     void addGenreToBook3(){
         int books = bookGenreDao.addGenreToBook(1,50);
         assertEquals(0,books);
 
     }
-
+    /**
+     * Test case for deleting genre to book.
+     * It asserts that the correct genres are deleted to the books given book ID and genre ID.
+     */
     @Test
     void deleteGenreToBook() {
  int books = bookGenreDao.deleteGenreToBook(5,2);
@@ -84,6 +114,10 @@ int books = bookGenreDao.addGenreToBook(5,1);
 
 
     }
+    /**
+     * Test case for deleting genre to book 2.
+     * Validates the wrong bookID.
+     */
     @Test
     void deleteGenreToBook2() {
         int books = bookGenreDao.deleteGenreToBook(50,2);
@@ -93,6 +127,10 @@ int books = bookGenreDao.addGenreToBook(5,1);
 
 
     }
+    /**
+     * Test case for deleting genre to book 3.
+     * Asserts equals if genre id doesnt exist .
+     */
     @Test
     void deleteGenreToBook3() {
         int books = bookGenreDao.deleteGenreToBook(5,50);
@@ -103,25 +141,42 @@ int books = bookGenreDao.addGenreToBook(5,1);
 
 
 
-
+    /**
+     * Test case finding duplicated genre in books.
+     * Asserts equals if genre id is duplicate.
+     */
     @Test
     void duplicateGenreInBook() {
 boolean books = bookGenreDao.duplicateGenreInBook(1,1);
 assertEquals(true,books);
 
     }
+    /**
+     * Test case finding duplicated genre in books 2.
+     * Asserts equals if genre isn't duplicate .
+     */
     @Test
     void duplicateGenreInBook2() {
         boolean books = bookGenreDao.duplicateGenreInBook(1,5);
         assertEquals(false,books);
 
     }
+    /**
+     * Test case finding duplicated genre in books 3.
+     * This test has invalid bookID.
+     * Asserts equals if genre isn't duplicate .
+     */
     @Test
     void duplicateGenreInBook3() {
         boolean books = bookGenreDao.duplicateGenreInBook(50,1);
         assertEquals(false,books);
 
     }
+    /**
+     * Test case finding duplicated genre in books 4.
+     * This test has invalid genreID.
+     * Asserts equals if genre isn't duplicate .
+     */
     @Test
     void duplicateGenreInBook4() {
         boolean books = bookGenreDao.duplicateGenreInBook(1,-1);
